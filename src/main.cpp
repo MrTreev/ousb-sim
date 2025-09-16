5dabe8cff93c903a0d0aa0258f63e1be21f1718e
MrTreev <mr.treev@gmail.com>
2025-09-16 23:28:21 +1000

    BATMAN

---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#include "ousb.h"
+#include <imgui.h>
+#include <rlImGui.h>
+
+class Application {
+    int  screenWidth  = 1280;
+    int  screenHeight = 800;
+    int  target_fps   = 60;
+    Ousb ousb{};
+
+public:
+    Application() {
+        SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+        InitWindow(screenWidth, screenHeight, "raylib-Extras [ImGui] example - simple ImGui Demo");
+        SetTargetFPS(target_fps);
+        rlImGuiSetup(true);
+    }
+
+    void draw_general_registers() const {
+        if (ImGui::Begin("Registers", nullptr)) {
+            ImGui::Text("R0:  0x%X", ousb.registers.R0());
+            ImGui::Text("R1:  0x%X", ousb.registers.R1());
+            ImGui::Text("R2:  0x%X", ousb.registers.R2());
+            ImGui::Text("R3:  0x%X", ousb.registers.R3());
+            ImGui::Text("R4:  0x%X", ousb.registers.R4());
+            ImGui::Text("R5:  0x%X", ousb.registers.R5());
+            ImGui::Text("R6:  0x%X", ousb.registers.R6());
+            ImGui::Text("R7:  0x%X", ousb.registers.R7());
+            ImGui::Text("R8:  0x%X", ousb.registers.R8());
+            ImGui::Text("R9:  0x%X", ousb.registers.R9());
+            ImGui::Text("R10: 0x%X", ousb.registers.R10());
+            ImGui::Text("R11: 0x%X", ousb.registers.R11());
+            ImGui::Text("R12: 0x%X", ousb.registers.R12());
+            ImGui::Text("R13: 0x%X", ousb.registers.R13());
+            ImGui::Text("R14: 0x%X", ousb.registers.R14());
+            ImGui::Text("R15: 0x%X", ousb.registers.R15());
+            ImGui::Text("R16: 0x%X", ousb.registers.R16());
+            ImGui::Text("R17: 0x%X", ousb.registers.R17());
+            ImGui::Text("R18: 0x%X", ousb.registers.R18());
+            ImGui::Text("R19: 0x%X", ousb.registers.R19());
+            ImGui::Text("R20: 0x%X", ousb.registers.R20());
+            ImGui::Text("R21: 0x%X", ousb.registers.R21());
+            ImGui::Text("R22: 0x%X", ousb.registers.R22());
+            ImGui::Text("R23: 0x%X", ousb.registers.R23());
+            ImGui::Text("R24: 0x%X", ousb.registers.R24());
+            ImGui::Text("R25: 0x%X", ousb.registers.R25());
+            ImGui::Text("R26: 0x%X", ousb.registers.R26());
+            ImGui::Text("R27: 0x%X", ousb.registers.R27());
+            ImGui::Text("R28: 0x%X", ousb.registers.R28());
+            ImGui::Text("R29: 0x%X", ousb.registers.R29());
+            ImGui::Text("R30: 0x%X", ousb.registers.R30());
+            ImGui::Text("R31: 0x%X", ousb.registers.R31());
+        }
+        ImGui::End();
+    }
+
+    void draw_io_registers() const {
+        if (ImGui::Begin("IO Registers", nullptr)) {
+            ImGui::Text("EEAR: 0x%X", ousb.io_registers.EEAR());
+        }
+        ImGui::End();
+    }
+
+    bool tick() {
+        if (WindowShouldClose()) return false;
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        rlImGuiBegin();
+        // NOLINTBEGIN(*-vararg,*-union-access)
+        draw_general_registers();
+        draw_io_registers();
+        // NOLINTEND(*-vararg,*-union-access)
+        rlImGuiEnd();
+        EndDrawing();
+        return true;
+    }
+};
+
+int main() {
+    Application app{};
+    // NOLINTNEXTLINE(*-signed-bitwise)
+    while (app.tick()) {}
+}
---
+++ src/ousb.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+// NOLINTBEGIN(*-magic-numbers)
+
+constexpr uint16_t uint8_to_16(uint8_t hi_val, uint8_t lo_val) {
+    return (static_cast<uint16_t>(static_cast<uint16_t>(hi_val) << 8U) | lo_val);
+}
+
+class Reg8 {
+    uint8_t m_val{0};
+
+public:
+    Reg8() = default;
+
+    explicit Reg8(uint8_t val)
+        : m_val(val) {}
+
+    uint8_t& operator()() { return m_val; }
+
+    uint8_t operator()() const { return m_val; }
+};
+
+class Reg16 {
+    std::array<uint8_t, 2> m_val{};
+
+public:
+    Reg16() = default;
+
+    Reg16(uint8_t hi_val, uint8_t lo_val)
+        : m_val({hi_val, lo_val}) {}
+
+    explicit Reg16(uint16_t val)
+        : m_val({static_cast<uint8_t>(val >> 8U), static_cast<uint8_t>(val & 0x00'FFU)}) {}
+
+    // NOLINTNEXTLINE(*-reinterpret-cast)
+    uint16_t& operator()() { return *reinterpret_cast<uint16_t*>(&m_val); }
+
+    uint8_t& hi() { return m_val[0]; }
+
+    uint8_t& lo() { return m_val[1]; }
+
+    [[nodiscard]]
+    uint16_t operator()() const {
+        return uint8_to_16(hi(), lo());
+    }
+
+    [[nodiscard]]
+    uint8_t hi() const {
+        return m_val[0];
+    }
+
+    [[nodiscard]]
+    uint8_t lo() const {
+        return m_val[1];
+    }
+};
+
+class Ousb {
+public:
+    static constexpr size_t flash_size = 0x3F'FFU;
+    static constexpr size_t sram_size  = 0x3F'FFU;
+
+    struct {
+        Reg8 R0;
+        Reg8 R1;
+        Reg8 R2;
+        Reg8 R3;
+        Reg8 R4;
+        Reg8 R5;
+        Reg8 R6;
+        Reg8 R7;
+        Reg8 R8;
+        Reg8 R9;
+        Reg8 R10;
+        Reg8 R11;
+        Reg8 R12;
+        Reg8 R13;
+        Reg8 R14;
+        Reg8 R15;
+        Reg8 R16;
+        Reg8 R17;
+        Reg8 R18;
+        Reg8 R19;
+        Reg8 R20;
+        Reg8 R21;
+        Reg8 R22;
+        Reg8 R23;
+        Reg8 R24;
+        Reg8 R25;
+        Reg8 R26;
+        Reg8 R27;
+        Reg8 R28;
+        Reg8 R29;
+        Reg8 R30;
+        Reg8 R31;
+
+        [[nodiscard]]
+        uint16_t X() const {
+            return uint8_to_16(R26(), R27());
+        }
+    } registers;
+
+    struct {
+        Reg16 EEAR{0xFF'FFU};
+        Reg8  EEDR;
+        Reg8  EECR;
+    } io_registers;
+
+    Reg16 stack_pointer;
+
+    std::array<uint8_t, flash_size> flash{};
+    std::array<uint8_t, sram_size>  sram{};
+};
